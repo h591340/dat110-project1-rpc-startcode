a41607e78dfeee4fb7075af80b342e11be60167b
h591340 <69825457+h591340@users.noreply.github.com>
2023-02-13 22:19:26 +0100

    Update MessagingServer.java

--- src/main/java/no/hvl/dat110/messaging/MessagingServer.java
+++ src/main/java/no/hvl/dat110/messaging/MessagingServer.java
@@ -2,6 +2,7 @@ package no.hvl.dat110.messaging;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 import no.hvl.dat110.TODO;
 
@@ -30,6 +31,19 @@ public class MessagingServer {
 
 		// TODO - START
 		// accept TCP connection on welcome socket and create messaging connection to be returned
+		try {
+			Socket clientSocket = welcomeSocket.accept();
+			connection=new MessageConnection(clientSocket);
+			
+			
+		}catch (IOException ex) {
+			System.out.println("TCP server: "+ex.getMessage());
+			ex.printStackTrace();
+			System.exit(1);
+			
+			
+		}
+		
 
 		if (true)
 			throw new UnsupportedOperationException(TODO.method());
