06cb2d03728d1d7930636f8d0dabc3b030b04a18
h591340 <69825457+h591340@users.noreply.github.com>
2023-03-02 21:32:14 +0100

    Update MessageUtils.java

--- src/main/java/no/hvl/dat110/messaging/MessageUtils.java
+++ src/main/java/no/hvl/dat110/messaging/MessageUtils.java
@@ -12,45 +12,38 @@ public class MessageUtils {
 	public static String MESSAGINGHOST = "localhost";
 
 	public static byte[] encapsulate(Message message) {
+		byte[]segment=null;
+		byte[]data;
 		
-		byte[] segment = null;
-		byte[] data;
+	
 		
 		// TODO - START
 		
 		// encapulate/encode the payload data of the message and form a segment
 		// according to the segment format for the messaging layer
 		data=message.getData();
+		segment=new byte[128];
+		segment[0]=(byte)data.length;
+		System.arraycopy(data, 0,segment, 1, data.length);
 		
-		
-		segment = new byte[data.length];
-		Arrays.fill(segment, (byte) 0);
-
-		for (int i = 0; i < data.length; i++) {
-		    segment[i] = data[i];
-		}
-		
+	
+		return segment;
 
 			
 		// TODO - END
-		return segment;
-		
 	}
+		
+
 
 	public static Message decapsulate(byte[] segment) {
+		Message message=null;
 
-		Message message = null;
-		
-		// TODO - START
-		// decapsulate segment and put received payload data into a message
-	
-		message=new Message(segment);
-		
-		
-		
-		// TODO - END
-		
-		return message;
+		 int dataLength = segment[0];
+		    byte[] data = new byte[dataLength];
+		    System.arraycopy(segment, 1, data, 0, dataLength);
+		    message=new Message(data);
+		    
+		   return message;
 		
 	}
 	
